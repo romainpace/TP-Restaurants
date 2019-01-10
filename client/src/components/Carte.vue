553d0c3020c95f9c55093910543609d57a63551c
romainpace <r-pace@hotmail.fr>
2019-01-10 01:00:02 +0100

    upload carte

--- client/src/components/Carte.vue
+++ client/src/components/Carte.vue
@@ -1,6 +1,30 @@
 <template>
 <div class="app">
 <h3>La carte </h3>
+
+<table>
+      <tr class="title">
+        <th>Entrées</th>
+        <th>Prix (€)</th>
+        <th>Description</th>
+        <th>Quantité</th>
+      </tr>
+      <tbody>
+        <tr class="content" v-for="e,index in listeEntree">
+
+        <td>{{ e.entree }}</td>
+        <td>{{ e.prix }}</td>
+        <td>{{ e.description }}</td>
+        <td>
+            <button v-on:click="incremente(index, listeEntree)">+</button>
+            <button  v-on:click="decremente(index, listeEntree)">-</button>
+            <input readonly type="number" name="quantite" min="0" max="50" v-bind:value=e.quantite>
+        </td>
+
+        </tr>
+      </tbody>
+    </table>
+
 	</div>
 </template>
 
@@ -22,7 +46,12 @@ export default {
              5,
              6,
              7
-          ]
+          ],
+        /*  description [
+          "Une salade toute simple",
+          "Asperges à l'huile",
+          "Crevettes avec un filet d'huile",
+          ]*/
       },
       plats: {
           plat: [
@@ -48,19 +77,62 @@ export default {
              5
           ]
       },
-
-      value_quantity:0,
     total:0,
-    shoppingCart : []
     }
   },
    mounted() {
             console.log("DETAIL VUE AVANT AFFICHAGE");
             this.getRestaurantsFromServer();
+            this.carteGenere(); 
         },
          methods :
     {
-      
+       getRestaurantsFromServer() {
+                let url = "http://localhost:8080/api/restaurants?page=" +
+                    this.page + "&pagesize=" +
+                    this.pagesize + "&name=" +
+                    this.name;
+
+                fetch(url)
+                    .then((reponseJSON) => {
+                        reponseJSON.json()
+                            .then((reponseJS) => {
+                                this.restaurants = reponseJS.data;
+                                this.nbRestaurants = reponseJS.count;
+                                this.dernierePage=Math.floor(this.nbRestaurants/this.pagesize);
+                                console.log(reponseJS.msg);
+                            });
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            },
+      carteGenere() {
+        var random;
+        var entre = "";
+        var carte = {};
+        var prix = 0;
+
+        for(var i= 0; i < 5; i++){
+            random = Math.floor(Math.random() * Math.floor(3));
+            entre = this.entrees.entree[random];
+            prix = this.entrees.prix[random];
+            carte = {'entree' : entre , 'prix': prix, quantite:0};
+            this.listeEntree.push(carte);
+        }
+    },
+    incremente(index, tab)
+  {
+        tab[index]['quantite']++;
+        this.total = this.total + tab[index]['prix'];
+    },
+  decremente(index, tab)
+  {
+        if(tab[index].quantite > 0){
+            tab[index].quantite--;
+            this.total = this.total - tab[index]['prix'];
+        }
+    },
       
     }
  
