c36f9405fc14caf4a3d763bfc61bf874d0cf8ac4
romainpace <r-pace@hotmail.fr>
2019-01-14 17:28:02 +0100

    App et main

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
 
- <router-link to="/restaurants">Restaurants</router-link>
+<!--  <router-link to="/restaurants">Restaurants</router-link> -->
+<app-restau></app-restau>
 <!--  <router-link to="/detail">Details</router-link> -->
     <router-view></router-view>
 
@@ -9,12 +10,16 @@
 </template>
 
 <script>
+	import Restaurants from './components/Restaurants.vue';
 export default {
   name: 'app',
   data () {
     return {
 
     }
+  },
+  components : {
+  	'app-restau' : Restaurants
   }
 }
 </script>
